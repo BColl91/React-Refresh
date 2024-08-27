150df2364e45b2cf002d9cd0c16bd52f3f4187b6
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-08-27 14:38:48 +0100

    caught up

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,33 @@
 import './App.css';
-import Box from './components/box';
 import { useState, useEffect } from 'react';
 
 function App() {
   // Normal JavaScript goes here
-  const [user, setUser] = useState(" ");
-  const characterArray = [{name:"George"},{name:"Bex"},{name:"Kate"}, {name:"Benji"}]
-  for (let index = 0; index < characterArray.length; index++) {
-    console.log(characterArray[index]);  
-  }
-  // The equivalent of this using map is ...
-  characterArray.map((item,index) => {console.log(item)})
+  const [toggle,setToggle] = useState(true);
+  const [imageArray, setImageArray] = useState([]);
 
   //FETCH
   const getImages = async () => {
     const response = await fetch("https://picsum.photos/v2/list");
     const data = await response.json();
-    console.log(data)
+    setImageArray(data);
   }
-  getImages();
+  useEffect(() => {getImages()},[])
   return (
     <div className="App">
-      <img src='https://picsum.photos/200' alt='a random photo'/>
+      <h1>List</h1>
+      <button onClick={(event) => {setToggle(!toggle);console.log(toggle)}}>Press me</button>
       <br></br>
-      <input onChange={(event) => setUser(event.target.value)}/>
-      {/* CONDITIONAL RENDERING */}
-      {user && <Box name ={user}/>}
-      {/* the {} allows use to inject JavaScript */}
-      {/* the && acts as an if statement so if use exists display what is to the right of the && and if user doesn't exist display nothing */}
-      { user==" " ? <Box name={user}/> : <h1>Nothing typed in</h1> }
-      {/* ? and : act as an if else statement*/}
-      <Box name ="Rk800"/>
-      {characterArray.map( (item, index) => { 
+      {imageArray.map( (item, index) => { 
         return(
           <div>
-              <Box name ={item.name} />
+
+              {toggle &&
+              <div> 
+              <img src={item.download_url} width={"50%"}/>
+              <h2>{item.author}</h2>
+              </div>
+              }
           </div>
         )
       })}
