f3838d5ec9db9898aa9399ad5b46b4d7c1d17371
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-08-27 13:49:41 +0100

    fixed error

--- src/App.jsx
+++ src/App.jsx
@@ -3,56 +3,41 @@ import Box from './components/box';
 import { useState, useEffect } from 'react';
 
 function App() {
+  // Normal JavaScript goes here
   const [user, setUser] = useState(" ");
-  const characterArray = [
-    { name: "George" },
-    { name: "Kate" },
-    { name: "notGeorge" },
-    { name: "Will" }
-  ];
-
-  // Original for loop with fixed array name
+  const characterArray = [{name:"George"},{name:"Bex"},{name:"Kate"}, {name:"Benji"}]
   for (let index = 0; index < characterArray.length; index++) {
-    console.log(characterArray[index]);    
+    console.log(characterArray[index]);  
   }
-
-  // The equivalent of the for loop using map
-  characterArray.map((item, index) => {console.log(item)});
+  // The equivalent of this using map is ...
+  characterArray.map((item,index) => {console.log(item)})
 
   //FETCH
   const getImages = async () => {
-    const response = await fetch("https://picsum.photos/v2/list")
+    const response = await fetch("https://picsum.photos/v2/list");
     const data = await response.json();
-    console.log(data);
-    
+    console.log(data)
   }
-getImages();
+  getImages();
   return (
-      //  <input onChange={(event) => setUser(event.target.value)} />
-      
-      // {/* CONDITIONAL RENDERING */}
-      // {user && <Box name={user} />}
-      // {/* the {} allows us to inspect Javascript. The && acts as an if statement,
-      //      so if user exists, display what is to the right of the &&;
-      //      if user doesn't exist, display nothing */}
-      
-      // {/* Ternary Operator for Conditional Rendering */}
-      // {user === "Bex" ? <Box name={user} /> : <h1>Nothing typed in</h1>}
-      // {/* ? and : act as an if-else statement */}
-      
-      // {/* Static Box component */}
-      // <Box name="Bex" />
-
-      // {/* Map through characterArray and render Boxes */}
-      // {characterArray.map((item, index) => {
-      //   return (
-      //     <div key={index}>
-      //       <Box name={item.name} />
-      //     </div>
-      //   );
-      // })}
-    <div>
-      {/* Static Boxes */}
+    <div className="App">
+      <img src='https://picsum.photos/200' alt='a random photo'/>
+      <br></br>
+      <input onChange={(event) => setUser(event.target.value)}/>
+      {/* CONDITIONAL RENDERING */}
+      {user && <Box name ={user}/>}
+      {/* the {} allows use to inject JavaScript */}
+      {/* the && acts as an if statement so if use exists display what is to the right of the && and if user doesn't exist display nothing */}
+      { user==" " ? <Box name={user}/> : <h1>Nothing typed in</h1> }
+      {/* ? and : act as an if else statement*/}
+      <Box name ="Rk800"/>
+      {characterArray.map( (item, index) => { 
+        return(
+          <div>
+              <Box name ={item.name} />
+          </div>
+        )
+      })}
     </div>
   );
 }
