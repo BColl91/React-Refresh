7964de784b6bc336d5712842dd6f1dd1ef7ad340
Rebecca Collins <rebeccacollins1007@gmail.com>
2024-08-28 11:43:34 +0100

    input components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import Register from './components/registerUser';
+import Login from './components/login';
 
 function App() {
   // Normal JavaScript goes here
   const [toggle,setToggle] = useState(true);
   const [imageArray, setImageArray] = useState([]);
 
+
+  
   //FETCH
   const getImages = async () => {
     const response = await fetch("https://picsum.photos/v2/list");
@@ -15,19 +19,18 @@ function App() {
   useEffect(() => {getImages()},[])
   return (
     <div className="App">
+      <Register/>
+      <Login />
       <h1>List</h1>
       <button onClick={(event) => {setToggle(!toggle);console.log(toggle)}}>Press me</button>
       <br></br>
       {imageArray.map( (item, index) => { 
         return(
           <div>
-
-              {toggle &&
-              <div> 
+              {toggle && <div> 
               <img src={item.download_url} width={"50%"}/>
               <h2>{item.author}</h2>
-              </div>
-              }
+              </div>}
           </div>
         )
       })}
